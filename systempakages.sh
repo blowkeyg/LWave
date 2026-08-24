3cab1d7d7ba9568addcf71c7e09703a92ebebc96
blowkeyg <stunkerm@icloud.com>
2026-08-24 15:02:23 -0500

    Add system packages installation script

---
+++ systempakages.sh
@@ -0,0 +1,3 @@
+linux linux-firmware systemd xorg-server xorg-xinit python flatpak ttf-jetbrains-mono
+openbox polybar plank rofi pipewire pipewire-pulse wireplumber mesa
+vulkan-intel xf86-video-intel vulkan-radeon xf86-video-amdgpu nvidia nvidia-utils nvidia-prime
